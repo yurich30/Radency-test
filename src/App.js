25593a4ff438821f812a9b39355cb1b09c37a9ce
Yurii <yurich3098@gmail.com>
2021-04-01 14:41:22 +0300

    add parseOptions for csvreader

--- src/App.js
+++ src/App.js
@@ -6,13 +6,23 @@ function App() {
 
 	const [workers, setWorkers] = React.useState([])
 	console.log(workers);
+	
+	const parseOptions = {
+		header: true,
+		dynamicTyping: true,
+		skipEmptyLines: true,
+		transformHeader: header =>
+		  header
+			.toLowerCase()
+			.replace(/\W/g, '_')
+	  }
 
   return (
     <div className="App">
 		<div>
 			<h1 className="App">Import Workers</h1>
 			<div className="imp">
-				<CSVReader onFileLoaded={(data) => setWorkers( prev => [...prev, ...data] )} />
+				<CSVReader  parserOptions={parseOptions} onFileLoaded={(data) => setWorkers( prev => [...prev, ...data] )} />
 			</div>
 		</div>
     </div>
