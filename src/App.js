fbd372ee917d5a26ff24fd148041b5d555ae5f59
Yurii <yurich3098@gmail.com>
2021-04-01 14:39:03 +0300

    create state and push data there

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import React from 'react'
 import CSVReader from 'react-csv-reader';
 import './App.css';
 
 function App() {
+
+	const [workers, setWorkers] = React.useState([])
+	console.log(workers);
+
   return (
     <div className="App">
 		<div>
 			<h1 className="App">Import Workers</h1>
 			<div className="imp">
-				<CSVReader onFileLoaded={(data, fileInfo) => console.dir(data, fileInfo)} />
+				<CSVReader onFileLoaded={(data) => setWorkers( prev => [...prev, ...data] )} />
 			</div>
 		</div>
     </div>
